85219e5ad9d59a6ecf5e66fb344fc142dba26c68
Guilherme Brito de Almeida Costa <guilhermebritoalmeidacosta@gmail.com>
2021-10-10 10:20:50 -0300

    Bloqueia o botão de submit quando a data estiver inválida

--- CRM_Crud/CRM_Crud/wwwroot/js/verificaData.js
+++ CRM_Crud/CRM_Crud/wwwroot/js/verificaData.js
@@ -1,16 +1,19 @@
 ﻿var data_inicio = document.getElementById("input_data_inicio")
 var data_termino = document.getElementById("input_data_termino")
 var label_data_termino = document.getElementById("label_data_termino")
+var btn_submit = document.getElementById("btn_submit")
 
 function Verificar()
 {
     if (data_inicio.value != "" && data_termino.value != "") {
         if (data_inicio.value > data_termino.value) {
             label_data_termino.innerHTML = "A data de termino precisa ser depois da data de inicio"
+            btn_submit.disabled = true
         }
         else
         {
             label_data_termino.innerHTML = ""
+            btn_submit.disabled = false
         }
     }
 }
